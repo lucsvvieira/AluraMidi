15549e72a79242fc08d8bd1cd418694c1ede01c6
Lucas Vieira <vieira07lucas@gmail.com>
2022-11-10 10:30:39 -0300

    Adicionando e removendo classes

--- main.js
+++ main.js
@@ -17,5 +17,9 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
   tecla.onkeydown = function () {
     tecla.classList.add('ativa');
   }
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove('ativa');
+  }
   
 }
