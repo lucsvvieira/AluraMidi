82392877aba3e0382f3c5633b9fd931290d1e5f0
Lucas Vieira <vieira07lucas@gmail.com>
2022-11-10 10:08:51 -0300

    Repetição otimizada com laço de repetição For

--- main.js
+++ main.js
@@ -4,18 +4,14 @@ function tocaSom(idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
-let contador = 0;
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; // Template string
 
-while (contador < listaDeTeclas.length) {
-  const tecla = listaDeTeclas[contador];
-  const instrumento = tecla.classList[1];
-
-  // Template string
-  const idAudio = `#som_${instrumento}`;
-
-  tecla.onclick = function () {
+    tecla.onclick = function () {
     tocaSom(idAudio);
   };
-
-  contador = contador + 1;
+  
 }
