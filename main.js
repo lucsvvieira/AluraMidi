241fc08be4b3b297ab58942e66d4ec15835b7f89
Lucas Vieira <vieira07lucas@gmail.com>
2022-11-18 11:08:54 -0300

    Adicionando operador lógico

--- main.js
+++ main.js
@@ -5,22 +5,20 @@ function tocaSom(idElementoAudio) {
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
-  
-    const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`; // Template string
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`; // Template string
 
-    tecla.onclick = function () {
-    tocaSom(idAudio)
+  tecla.onclick = function () {
+    tocaSom(idAudio);
   };
 
   tecla.onkeydown = function (evento) {
-    if (evento.code === 'Space')
-    tecla.classList.add('ativa');
-  }
+    if (evento.code === "Space" || evento.code === "Enter")
+      tecla.classList.add("ativa");
+  };
 
   tecla.onkeyup = function () {
-    tecla.classList.remove('ativa');
-  }
-  
+    tecla.classList.remove("ativa");
+  };
 }
